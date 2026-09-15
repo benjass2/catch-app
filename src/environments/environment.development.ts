59ffe3434c06765b7458e5300d9c83b694696fb3
benjass2 <benjalicket@gmail.com>
2026-09-15 11:34:28 -0500

    feat(environments):  add environments 1

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+/**
+ *Development environment
+ * */
+
+export const environment = {
+  production: false,
+  newsProdiverApiBaseUrl: 'http://newsapi.org/v2',
+  newProvuderNewsEndpointpath: '/top-headlines',
+  newsProviderSourcesEndpointPath: 'top-headlines/sources',
+  newProviderApiKey: 'YOUR_NEWS_PROVIDER_API_KEY',
+  logoProviderApiBaseUrl: 'http://img.logo.dev',
+  logoProviderPublishableKey: 'YOUR_LOGO_API_KEY',
+};
